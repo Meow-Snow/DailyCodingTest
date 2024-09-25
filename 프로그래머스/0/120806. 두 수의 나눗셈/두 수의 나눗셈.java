ba3e369f8b563b032cff7979e295e0c0c226a162
Meow-Snow <MeowSnow7771@gmail.com>
2024-09-26 06:02:53 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.03 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -1,5 +1,6 @@
 class Solution {
     public int solution(int num1, int num2) {
-        return 1000 * num1 / num2;
+        return (int) ((double) num1 / num2 * 1000);
+        // return 1000 * num1 / num2;
     }
 }
