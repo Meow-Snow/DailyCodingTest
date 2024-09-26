755ddd03eab364fe87cf50c26b28e19dc15ba554
Meow-Snow <MeowSnow7771@gmail.com>
2024-09-27 03:46:29 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -1,8 +1,16 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        for (int i = 0; i <= n; i += 2) answer += i;
-        
-        return answer;
+        return n / 2 * (n / 2 + 1);
     }
 }
+
+
+
+// class Solution {
+//     public int solution(int n) {
+//         int answer = 0;
+//         for (int i = 0; i <= n; i += 2) answer += i;
+        
+//         return answer;
+//     }
+// }
