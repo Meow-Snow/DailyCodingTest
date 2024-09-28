947ac842b07ccb253d8b45ec0ab4d562dab9770c
Meow-Snow <MeowSnow7771@gmail.com>
2024-09-29 04:28:57 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.java
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -1,17 +1,8 @@
 class Solution {
     public int solution(int angle) {
-        return switch (angle) {
-            case 90 -> 2;
-            case 180 -> 4;
-            default -> (angle < 90)? 1 : 3;
-        };
+        return (angle < 90)? 1 : (angle == 90)? 2 : (angle == 180)? 4 : 3;
     }
 }
 
 
 
-// class Solution {
-//     public int solution(int angle) {
-//         return (angle < 90)? 1 : (angle == 90)? 2 : (angle == 180)? 4 : 3;
-//     }
-// }
