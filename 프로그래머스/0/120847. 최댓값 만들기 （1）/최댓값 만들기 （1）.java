4e7ff7341f44b20461629e2dbfd3bf6571412da9
Meow-Snow <MeowSnow7771@gmail.com>
2024-09-30 23:52:06 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.52 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -1,16 +1,10 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] numbers) {
-        int max1 = 0;
-        int max2 = 0;
+        int len = numbers.length;
+        Arrays.sort(numbers);
         
-        for (int number : numbers) {
-            if (number > max1) {
-                max2 = max1;
-                max1 = number;
-            }
-            else if (number > max2) max2 = number;                
-        }
-        
-        return max1 * max2;
+        return numbers[len - 1] * numbers[len - 2];
     }
 }
