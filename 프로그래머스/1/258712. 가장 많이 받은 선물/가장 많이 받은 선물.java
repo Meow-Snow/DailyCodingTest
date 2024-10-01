0e56e4268f84785bdeafc63b1f604730cd388614
Meow-Snow <MeowSnow7771@gmail.com>
2024-10-01 16:06:59 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 43.03 ms, Memory: 116 MB -BaekjoonHub

---
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] friends, String[] gifts) {
+        HashMap<String, Integer> idMap = new HashMap<>();
+        for (int i = 0; i < friends.length; i++) idMap.put(friends[i], i);
+        
+        HashMap<String, int[]> giftMap = new HashMap<>();
+        for (String friend : friends) {
+            int[] giftArr = new int[friends.length];
+            giftMap.put(friend, giftArr);
+        }
+        
+        int[] giftIdxArr = new int[friends.length];
+        for (String gift : gifts) {
+            String[] exchangers = gift.split("\\s+");
+            giftMap.get(exchangers[0])[idMap.get(exchangers[1])]++;
+            giftIdxArr[idMap.get(exchangers[0])]++;
+            giftIdxArr[idMap.get(exchangers[1])]--;
+        }
+        
+        int maxGiftCnt = 0;
+        for (int i = 0; i < friends.length; i++) {
+            int giftCnt = 0;
+            for (int j = 0; j < friends.length; j++) {
+                if (i == j) continue;                
+                int giveCnt = giftMap.get(friends[i])[j];
+                int takeCnt = giftMap.get(friends[j])[i];
+                if (giveCnt > takeCnt || giveCnt == takeCnt && (giftIdxArr[i] > giftIdxArr[j])) giftCnt++;
+            }            
+            maxGiftCnt = Math.max(maxGiftCnt, giftCnt);
+        }
+        
+        return maxGiftCnt;
+    }
+}
