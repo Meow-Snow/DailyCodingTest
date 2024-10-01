45c16c179f0ca13d0bd4866609014276cb1d22d9
Meow-Snow <MeowSnow7771@gmail.com>
2024-10-01 20:15:50 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 15.59 ms, Memory: 94.1 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -2,27 +2,27 @@ import java.util.*;
 
 class Solution {
     public int solution(String[] friends, String[] gifts) {
+        int len = friends.length;
+            
         HashMap<String, Integer> idMap = new HashMap<>();
-        for (int i = 0; i < friends.length; i++) idMap.put(friends[i], i);
-
-        HashMap<String, int[]> giftMap = new HashMap<>();
-        for (String friend : friends) giftMap.put(friend, new int[friends.length]);
-
-        int[] giftIdxArr = new int[friends.length];
+        for (int i = 0; i < len; i++) idMap.put(friends[i], i);
+        
+        int[][] giftArr = new int[len][len];
+        int[] giftIdxArr = new int[len];
         for (String gift : gifts) {
-            String[] exchangers = gift.split("\\s+");
-            giftMap.get(exchangers[0])[idMap.get(exchangers[1])]++;
+            String[] exchangers = gift.split(" ");
+            giftArr[idMap.get(exchangers[0])][idMap.get(exchangers[1])]++;
             giftIdxArr[idMap.get(exchangers[0])]++;
             giftIdxArr[idMap.get(exchangers[1])]--;
         }
 
         int maxGiftCnt = 0;
-        for (int i = 0; i < friends.length; i++) {
+        for (int i = 0; i < len; i++) {
             int giftCnt = 0;
-            for (int j = 0; j < friends.length; j++) {
+            for (int j = 0; j < len; j++) {
                 if (i == j) continue;                
-                int giveCnt = giftMap.get(friends[i])[j];
-                int takeCnt = giftMap.get(friends[j])[i];
+                int giveCnt = giftArr[i][j];
+                int takeCnt = giftArr[j][i];
                 if (giveCnt > takeCnt || giveCnt == takeCnt && (giftIdxArr[i] > giftIdxArr[j])) giftCnt++;
             }         
             maxGiftCnt = Math.max(maxGiftCnt, giftCnt);
@@ -31,3 +31,41 @@ class Solution {
         return maxGiftCnt;
     }
 }
+
+
+
+
+
+// import java.util.*;
+
+// class Solution {
+//     public int solution(String[] friends, String[] gifts) {
+//         HashMap<String, Integer> idMap = new HashMap<>();
+//         for (int i = 0; i < friends.length; i++) idMap.put(friends[i], i);
+
+//         HashMap<String, int[]> giftMap = new HashMap<>();
+//         for (String friend : friends) giftMap.put(friend, new int[friends.length]);
+
+//         int[] giftIdxArr = new int[friends.length];
+//         for (String gift : gifts) {
+//             String[] exchangers = gift.split("\\s+");
+//             giftMap.get(exchangers[0])[idMap.get(exchangers[1])]++;
+//             giftIdxArr[idMap.get(exchangers[0])]++;
+//             giftIdxArr[idMap.get(exchangers[1])]--;
+//         }
+
+//         int maxGiftCnt = 0;
+//         for (int i = 0; i < friends.length; i++) {
+//             int giftCnt = 0;
+//             for (int j = 0; j < friends.length; j++) {
+//                 if (i == j) continue;                
+//                 int giveCnt = giftMap.get(friends[i])[j];
+//                 int takeCnt = giftMap.get(friends[j])[i];
+//                 if (giveCnt > takeCnt || giveCnt == takeCnt && (giftIdxArr[i] > giftIdxArr[j])) giftCnt++;
+//             }         
+//             maxGiftCnt = Math.max(maxGiftCnt, giftCnt);
+//         }
+
+//         return maxGiftCnt;
+//     }
+// }
