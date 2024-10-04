1c908d214070476cc0ed676b4289690849fc999d
Meow-Snow <MeowSnow7771@gmail.com>
2024-10-04 16:48:45 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.06 ms, Memory: 70.5 MB -BaekjoonHub

--- 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -1,6 +1,10 @@
 class Solution {
     public String solution(String my_string, int n) {
-        String answer = "";
-        return answer;
+        StringBuilder sb = new StringBuilder();
+        for (char c : my_string.toCharArray()) {
+            for (int i = 0; i < n; i++) sb.append(c);
+        }
+        
+        return sb.toString();
     }
 }
