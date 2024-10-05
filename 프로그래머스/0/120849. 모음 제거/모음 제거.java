bf087727692d2a7d3d4ef2d22fa11219de29dd0e
Meow-Snow <MeowSnow7771@gmail.com>
2024-10-05 16:27:10 +0900

    [level 0] Title: 모음 제거, Time: 0.15 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,10 +1,20 @@
 class Solution {
     public String solution(String my_string) {
-        String[] strArr = {"a", "e", "i", "o", "u"};
-        for (String str : strArr) {
-            my_string = my_string.replace(str, "");
-        }
-        
-        return my_string;
+        return my_string.replaceAll("[aeiou]", "");
     }
 }
+
+
+
+
+
+// class Solution {
+//     public String solution(String my_string) {
+//         String[] strArr = {"a", "e", "i", "o", "u"};
+//         for (String str : strArr) {
+//             my_string = my_string.replace(str, "");
+//         }
+        
+//         return my_string;
+//     }
+// }
