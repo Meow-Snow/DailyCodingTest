34df556f93c79f24c7e4112b607771d0a46551d2
Meow-Snow <MeowSnow7771@gmail.com>
2024-10-06 17:36:20 +0900

    [level 1] Title: x만큼 간격이 있는 n개의 숫자, Time: 0.06 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.java
+++ 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.java
@@ -1,3 +1,8 @@
+// Array의 index로는 int만 사용 가능
+// int -> size = 32 bit (4 byte), range = -2^31 ~ 2^31 - 1 (약 -21.5억 ~ 21.5억)
+// long -> size = 64 bit (8 byte), range = -2^63 ~ 2^63 - 1
+// 10,000,000 * 1,000 = 100억 이므로 int의 범위를 벗어나므로 long을 써야 한다
+
 class Solution {
     public long[] solution(int x, int n) {
         long[] answer = new long[n];
