e7d7d14cb87adba0996f6fd3b5d0248404f0cb46
Meow-Snow <MeowSnow7771@gmail.com>
2024-10-12 01:48:03 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.06 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,7 @@
+class Solution {
+    public long solution(long n) {
+        double sqrt = Math.sqrt(n);
+        
+        return (sqrt == (int) sqrt)? (long) Math.pow(sqrt + 1, 2) : -1;
+    }
+}
