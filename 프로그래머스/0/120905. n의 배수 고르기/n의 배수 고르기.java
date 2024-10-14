41e10e5ee7b993ba782ee38be4368d404b8846cc
Meow-Snow <MeowSnow7771@gmail.com>
2024-10-14 09:44:49 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.06 ms, Memory: 70.4 MB -BaekjoonHub

--- 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.java
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -16,3 +16,13 @@ class Solution {
         return answer;
     }
 }
+
+
+
+// import java.util.Arrays;
+
+// class Solution {
+//     public int[] solution(int n, int[] numlist) {
+//         return Arrays.stream(numlist).filter(i -> i % n == 0).toArray();
+//     }
+// }
