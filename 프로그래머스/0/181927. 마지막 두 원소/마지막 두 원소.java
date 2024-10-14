e306304001934c65bae6ccb68205bfafa58d0ced
Meow-Snow <MeowSnow7771@gmail.com>
2024-10-15 07:51:54 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.04 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -2,9 +2,10 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = Arrays.copyOfRange(num_list, 0, num_list.length + 1);
-        int len = answer.length;
-        answer[len - 1] = (answer[len - 2] > answer[len - 3])? answer[len - 2] - answer[len - 3] : answer[len - 2] * 2;
+        int last = num_list[num_list.length - 1];
+        int prev = num_list[num_list.length - 2];
+        int[] answer = Arrays.copyOfRange(num_list, 0, num_list.length + 1);        
+        answer[answer.length - 1] = (last > prev)? last - prev : last * 2;
             
         return answer;
     }
