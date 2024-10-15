9eb86ca42370617ffece05b609a0386b9b07c695
Meow-Snow <MeowSnow7771@gmail.com>
2024-10-16 00:55:04 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.03 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int sum = 0;
+        int product = 1;
+        for (int num : num_list) {
+            sum += num;
+            product *= num;
+        }
+        
+        return (sum * sum < product)? 0 : 1;
+    }
+}
