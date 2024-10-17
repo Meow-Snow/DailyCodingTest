82e8d44e7fbdce35278eccb1eab49246a54b931b
Meow-Snow <MeowSnow7771@gmail.com>
2024-10-18 04:34:24 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.10 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/README.md
+++ 프로그래머스/1/12930. 이상한 문자 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 이상한 문자 만들기 - 12930 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12930) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12930#) 
 
 ### 성능 요약
 
-메모리: 75.2 MB, 시간: 0.10 ms
+메모리: 72.6 MB, 시간: 0.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 18일 03:54:40
+2024년 10월 18일 04:34:22
 
 ### 문제 설명
 
--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -13,3 +13,19 @@ class Solution {
         return answer.toString();
     }
 }
+
+
+
+// class Solution {
+//     public String solution(String s) {
+//         char[] charArr = s.toCharArray();
+//         int wordIdx = 0;                
+//         for (int i = 0; i < charArr.length; i++) {
+//             char c = charArr[i];
+//             if (c == ' ') wordIdx = 0;
+//             else charArr[i] = (wordIdx++ % 2 == 0)? Character.toUpperCase(c) : Character.toLowerCase(c);
+//         }
+        
+//         return String.valueOf(charArr);
+//     }
+// }
