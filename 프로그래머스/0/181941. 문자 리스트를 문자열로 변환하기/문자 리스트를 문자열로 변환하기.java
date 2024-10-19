a3df805e1371030614c778ab2ba3eac8f336a276
Meow-Snow <MeowSnow7771@gmail.com>
2024-10-20 00:46:41 +0900

    [level 0] Title: 문자 리스트를 문자열로 변환하기, Time: 0.38 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/0/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.java
+++ 프로그래머스/0/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.java
@@ -1,13 +1,22 @@
+// String.join(delimeter, String[]) : 문자열 배열의 원소들 사이에 구분자를 넣어서 연결한 하나의 문자열을 반환
 class Solution {
     public String solution(String[] arr) {
-        char[] charArr = new char[arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            charArr[i] = arr[i].charAt(0);
-        }
-        
-        return String.valueOf(charArr);
+        return String.join("", arr);
     }
 }
+
+
+
+// class Solution {
+//     public String solution(String[] arr) {
+//         char[] charArr = new char[arr.length];
+//         for (int i = 0; i < arr.length; i++) {
+//             charArr[i] = arr[i].charAt(0);
+//         }
+        
+//         return String.valueOf(charArr);
+//     }
+// }
 
 
 
