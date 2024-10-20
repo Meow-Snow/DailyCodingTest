e0ebc75f20c89a3b39b127d392a6b802588d2e5c
Meow-Snow <MeowSnow7771@gmail.com>
2024-10-21 03:29:28 +0900

    [level 1] Title: 명예의 전당 (1), Time: 2.00 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,22 +1,43 @@
-import java.util.*;
+// PriorityQueue(=우선순위큐)를 활용하면 간단히 해결 가능
+// PriorityQueue에 요소를 추가하면 MinHeap(최소 힙)을 기반으로 내부적으로 자동 정렬되어 가장 작은 요소가 큐의 root에 위치
+import java.util.PriorityQueue;
 
 class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
-        List<Integer> scoreList = new ArrayList<>();
-        scoreList.add(0);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         
         for (int i = 0; i < score.length; i++) {
-            for (int j = 0; j <= k; j++) {
-                if (score[i] >= scoreList.get(j)) {
-                    scoreList.add(j, score[i]);
-                    break;
-                }
-            }
-            
-            answer[i] = (i < k)? scoreList.get(i) : scoreList.get(k - 1);
+            pq.offer(score[i]);
+            if (pq.size() > k) pq.poll();
+            answer[i] = pq.peek();
         }
         
         return answer;
     }
 }
+
+
+
+// import java.util.*;
+
+// class Solution {
+//     public int[] solution(int k, int[] score) {
+//         int[] answer = new int[score.length];
+//         List<Integer> scoreList = new ArrayList<>();
+//         scoreList.add(0);
+        
+//         for (int i = 0; i < score.length; i++) {
+//             for (int j = 0; j <= k; j++) {
+//                 if (score[i] >= scoreList.get(j)) {
+//                     scoreList.add(j, score[i]);
+//                     break;
+//                 }
+//             }
+            
+//             answer[i] = (i < k)? scoreList.get(i) : scoreList.get(k - 1);
+//         }
+        
+//         return answer;
+//     }
+// }
