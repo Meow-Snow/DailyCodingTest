45dde33c651dc8f95c8785d53712d4f6244e7544
Meow-Snow <MeowSnow7771@gmail.com>
2024-10-22 15:01:58 +0900

    [level 0] Title: 주사위의 개수, Time: 0.03 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
@@ -1,5 +1,18 @@
 class Solution {
-    public int solution(int[] box, int n) {        
-        return (box[0] / n) * (box[1] / n) * (box[2] / n);
+    public int solution(int[] box, int n) {
+        int maxCnt = 1;
+        for (int length : box) {
+            maxCnt *= length / n;
+        }
+        
+        return maxCnt;
     }
 }
+
+
+
+// class Solution {
+//     public int solution(int[] box, int n) {        
+//         return (box[0] / n) * (box[1] / n) * (box[2] / n);
+//     }
+// }
