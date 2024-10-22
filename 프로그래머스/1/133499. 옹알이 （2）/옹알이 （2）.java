f42b92cebcf271b7801aa1bbea01ee2045392620
Meow-Snow <MeowSnow7771@gmail.com>
2024-10-23 04:31:39 +0900

    [level 1] Title: 옹알이 (2), Time: 1.70 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -1,3 +1,7 @@
+// 조카가 발음할 수 있는 단어(validBabbling)를 바로 빈 문자열("", empty string)으로 대체할 경우 
+// 그 단어 앞뒤의 문자들이 결합하여 원래 옹알이(babbling)에는 존재하지 않는 발음할 수 있는 단어를 형성 가능
+// 따라서 일차적으로 공백 문자열(" ", space string)로 대체한 이후 마지막에 공백 문자열들을 빈 문자열로 대체
+
 class Solution {
     public int solution(String[] babblings) {
         int answer = 0;
