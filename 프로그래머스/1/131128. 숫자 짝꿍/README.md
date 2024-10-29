1470b7fd409d4cb0cea99d4814ec6dfc4fe35fb6
Meow-Snow <MeowSnow7771@gmail.com>
2024-10-30 08:09:18 +0900

    [level 1] Title: 숫자 짝꿍, Time: 118.45 ms, Memory: 144 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/README.md
+++ 프로그래머스/1/131128. 숫자 짝꿍/README.md
@@ -1,10 +1,10 @@
 # [level 1] 숫자 짝꿍 - 131128 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131128) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131128#) 
 
 ### 성능 요약
 
-메모리: 157 MB, 시간: 111.52 ms
+메모리: 144 MB, 시간: 118.45 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 30일 07:23:36
+2024년 10월 30일 08:09:14
 
 ### 문제 설명
 
--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -22,3 +22,43 @@ class Solution {
         return numCntArr;
     }
 }
+
+
+
+// 숫자를 숫자형 문자로 변환하는 법
+// 1. Character.forDigit(digit, radix);
+// 2. (char) ('0' + digit);
+
+// 숫자형 문자를 숫자로 변환하는 법
+// 1. Character.getNumericValue(digitChar);
+// 2. (int) (digitChar - '0');
+
+// Map을 이용한 풀이 : Array를 이용했을 때보다 처리속도가 느리고, 코드가 약간 더 복잡해진다
+// import java.util.*;
+
+// class Solution {
+//     public String solution(String X, String Y) {
+//         Map<Character, Integer> numCntMapX = getNumCntMap(X);
+//         Map<Character, Integer> numCntMapY = getNumCntMap(Y);
+        
+//         StringBuilder sb = new StringBuilder();
+//         for (int i = 9; i >= 0; i--) {
+//             int numCntX = numCntMapX.getOrDefault(Character.forDigit(i, 10), 0);
+//             int numCntY = numCntMapY.getOrDefault((char) ('0' + i), 0);
+//             for (int j = 0; j < Math.min(numCntX, numCntY); j++) {
+//                 sb.append(i);
+//             }
+//         }
+                
+//         return (sb.length() == 0)? "-1" : (sb.charAt(0) == '0')? "0" : sb.toString();
+//     }
+    
+//     private Map<Character, Integer> getNumCntMap(String nums) {
+//         Map<Character, Integer> numCntMap = new HashMap<Character, Integer>();
+//         for (char num : nums.toCharArray()) {
+//             numCntMap.put(num, numCntMap.getOrDefault(num, 0) + 1);
+//         }
+        
+//         return numCntMap;
+//     }
+// }
