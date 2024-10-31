ddaca7f1e55230e3646345100a275d49725146f9
Meow-Snow <MeowSnow7771@gmail.com>
2024-11-01 06:55:38 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 지폐 접기, Time: 0.06 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.java
+++ 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.java
@@ -1,12 +1,12 @@
 class Solution {
     public int solution(int[] wallet, int[] bill) {
-        wallet = alignDimensions(wallet);
-        bill = alignDimensions(bill);
+        wallet = sort(wallet);
+        bill = sort(bill);
         
         int answer = 0;
         while (bill[0] > wallet[0] || bill[1] > wallet[1]) {
             bill[1] /= 2;
-            bill = alignDimensions(bill);
+            bill = sort(bill);
             
             answer++;
         }
@@ -14,7 +14,7 @@ class Solution {
         return answer;
     }
     
-    private int[] alignDimensions(int[] something) {
+    private int[] sort(int[] something) {
         return new int[] {Math.min(something[0], something[1]), Math.max(something[0], something[1])};
     }
 }
