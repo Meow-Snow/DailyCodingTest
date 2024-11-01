ace19b28ec49169d981415db62474aa132a8d97f
Meow-Snow <MeowSnow7771@gmail.com>
2024-11-02 07:00:28 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.64 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+class Solution {
+    private static final int[][] KEY_PAD = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {-1, 0, -2}};
+    private static final Set<Integer> LEFT_KEYS = new HashSet<>(Arrays.asList(1, 4, 7));
+    private static final Set<Integer> RIGHT_KEYS = new HashSet<>(Arrays.asList(3, 6, 9));
+    
+    public String solution(int[] numbers, String hand) {
+        Map<Integer, int[]> keyMap = new HashMap<>();        
+        for (int i = 0; i < KEY_PAD.length; i++) {
+            for (int j = 0; j < KEY_PAD[i].length; j++) {
+                keyMap.put(KEY_PAD[i][j], new int[]{i, j});
+            }
+        }
+                                        
+        hand = hand.equals("left")? "L" : "R";
+        int keyLeftHand = -1;
+        int keyRightHand = -2;
+        StringBuilder sb = new StringBuilder();
+        for (int num : numbers) {
+            if (LEFT_KEYS.contains(num)) {
+                sb.append("L");
+                keyLeftHand = num;
+            } else if (RIGHT_KEYS.contains(num)) {
+                sb.append("R");
+                keyRightHand = num;
+            } else {
+                int distanceLeftHand = getDistance(keyMap, keyLeftHand, num);
+                int distanceRightHand = getDistance(keyMap, keyRightHand, num);
+                
+                if (distanceLeftHand < distanceRightHand) {
+                    sb.append("L");
+                    keyLeftHand = num;
+                } else if (distanceLeftHand > distanceRightHand) {
+                    sb.append("R");
+                    keyRightHand = num;
+                } else {
+                    sb.append(hand);
+                    if (hand.equals("L")) {
+                        keyLeftHand = num;
+                    } else {
+                        keyRightHand = num;
+                    }
+                }
+            }
+        }
+        
+        return sb.toString();
+    }
+    
+    private int getDistance(Map<Integer, int[]> keyMap, int curKey, int nextKey) {
+        int[] curKeyPos = keyMap.get(curKey);
+        int[] nextKeyPos = keyMap.get(nextKey);
+        
+        return Math.abs(curKeyPos[0] - nextKeyPos[0]) + Math.abs(curKeyPos[1] - nextKeyPos[1]);
+    }
+}
