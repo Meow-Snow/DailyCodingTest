0b9087f5c32e00f04d9cecff2eb49d55ea3c588b
Meow-Snow <MeowSnow7771@gmail.com>
2024-11-15 02:44:45 +0900

    [level 0] Title: 배열 비교하기, Time: 0.04 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int[] arr1, int[] arr2) {
+        if (arr1.length == arr2.length) {
+            int sum1 = getSum(arr1);
+            int sum2 = getSum(arr2);
+            
+            if (sum1 > sum2) return 1;
+            else if (sum1 < sum2) return -1;
+            else return 0;
+        }
+        
+        return (arr1.length > arr2.length)? 1 : -1;
+    }
+    
+    private int getSum(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        
+        return sum;
+    }
+}
