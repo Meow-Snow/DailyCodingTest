ce1651d9bda3a07b30cc325a61a2ee119b47efd2
Meow-Snow <MeowSnow7771@gmail.com>
2024-11-17 10:08:22 +0900

    [level 0] Title: 5명씩, Time: 0.03 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.java
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -1,9 +1,8 @@
 class Solution {
     public String[] solution(String[] names) {
         String[] answer = new String[(names.length + 4) / 5];
-        int idx = 0;
         for (int i = 0; i < names.length; i += 5) {
-            answer[idx++] = names[i];
+            answer[i / 5] = names[i];
         }
         
         return answer;
