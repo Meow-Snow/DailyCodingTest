3635954b7e91313401942684b5fbf047c3090f69
Meow-Snow <MeowSnow7771@gmail.com>
2024-11-17 10:07:15 +0900

    [level 0] Title: 5명씩, Time: 0.03 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.java
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -2,10 +2,8 @@ class Solution {
     public String[] solution(String[] names) {
         String[] answer = new String[(names.length + 4) / 5];
         int idx = 0;
-        for (int i = 0; i < names.length; i++) {
-            if (i % 5 == 0) {
-                answer[idx++] = names[i];
-            }
+        for (int i = 0; i < names.length; i += 5) {
+            answer[idx++] = names[i];
         }
         
         return answer;
--- 프로그래머스/0/181886. 5명씩/README.md
+++ 프로그래머스/0/181886. 5명씩/README.md
@@ -1,10 +1,10 @@
 # [level 0] 5명씩 - 181886 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181886) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181886#) 
 
 ### 성능 요약
 
-메모리: 81.9 MB, 시간: 0.03 ms
+메모리: 77.7 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 17일 09:36:11
+2024년 11월 17일 10:07:11
 
 ### 문제 설명
 
