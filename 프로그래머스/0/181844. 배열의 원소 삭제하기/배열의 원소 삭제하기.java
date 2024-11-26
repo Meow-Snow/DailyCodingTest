57e38dc329d37dc558e9676d9fc1bc0ada70e4bd
Meow-Snow <MeowSnow7771@gmail.com>
2024-11-27 03:57:44 +0900

    [level 0] Title: 배열의 원소 삭제하기, Time: 0.55 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.java
+++ 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.java
@@ -1,3 +1,8 @@
+// List.remove()는 매개변수로 primitive type인 int가 들어가면 해당 리스트의 index로 인식하고
+// ex) List.remove(3) -> list에서 index가 3인 데이터를 삭제
+// boxed type인 Integer가 들어가면 index가 아닌 값 그 자체로 인식한다
+// ex) List.remove(Integer.valueOf(3)) -> list에서 값이 3인 데이터를 삭제
+
 import java.util.*;
 
 class Solution {
