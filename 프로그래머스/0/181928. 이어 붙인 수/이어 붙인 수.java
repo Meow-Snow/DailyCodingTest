a96abc7053c9c1a8ac999431cb9a6153f5bed3b5
Meow-Snow <MeowSnow7771@gmail.com>
2024-12-01 04:28:34 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.08 ms, Memory: 89.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] num_list) {
+        StringBuilder odd = new StringBuilder();
+        StringBuilder even = new StringBuilder();
+        for (int num : num_list) {
+            if (num % 2 != 0) odd.append(num);
+            else even.append(num);
+        }
+        
+        return Integer.parseInt(odd.toString()) + Integer.valueOf(even.toString());
+    }
+}
