af70fcaa3df6977cc01c93ba28cbd5a72b8187c7
Meow-Snow <MeowSnow7771@gmail.com>
2024-12-05 03:52:39 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.02 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.java
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.java
@@ -1,14 +1,16 @@
 class Solution {
     public int[] solution(int[] numbers, String direction) {
         int len = numbers.length;
-        int[] answer1 = new int[len];
-        int[] answer2 = new int[len];
+        int[] answer = new int[len];
         
-        System.arraycopy(numbers, 0, answer1, 1, len - 1);
-        answer1[0] = numbers[len - 1];
-        System.arraycopy(numbers, 1, answer2, 0, len - 1);
-        answer2[len - 1] = numbers[0];
+        if (direction.equals("right")) {
+            System.arraycopy(numbers, 0, answer, 1, len - 1);
+            answer[0] = numbers[len - 1];            
+        } else {
+            System.arraycopy(numbers, 1, answer, 0, len - 1);
+            answer[len - 1] = numbers[0];            
+        }        
         
-        return (direction.equals("right"))? answer1 : answer2;
+        return answer;
     }
 }
