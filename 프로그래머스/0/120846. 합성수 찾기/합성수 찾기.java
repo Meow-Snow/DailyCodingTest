b73c9726d8fdb22205ff15706934694d4e9db24d
Meow-Snow <MeowSnow7771@gmail.com>
2024-12-10 10:08:45 +0900

    [level 0] Title: 합성수 찾기, Time: 0.05 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
@@ -1,19 +1,41 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        for (int i = 1; i <= n; i++) {
-            if (getDivCnt(i) >= 3) answer++;
+        for (int i = 2; i <= n; i++) {
+            if (isComposite(i)) answer++;
         }
         
         return answer;
     }
     
-    private int getDivCnt(int number) {
-        int divCnt = 0;
-        for (int i = 1; i < Math.sqrt(number); i++) {
-            if (number % i == 0) divCnt += 2;
+    private boolean isComposite(int number) {
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) return true;
         }
         
-        return (int) Math.sqrt(number) == Math.sqrt(number)? divCnt + 1 : divCnt;
+        return false;
     }
 }
+
+
+
+// 굳이 약수의 개수를 모두 구해서 합성수인지 판단하는 방식은 비효율적이다
+// class Solution {
+//     public int solution(int n) {
+//         int answer = 0;
+//         for (int i = 1; i <= n; i++) {
+//             if (getDivCnt(i) >= 3) answer++;
+//         }
+        
+//         return answer;
+//     }
+    
+//     private int getDivCnt(int number) {
+//         int divCnt = 0;
+//         for (int i = 1; i < Math.sqrt(number); i++) {
+//             if (number % i == 0) divCnt += 2;
+//         }
+        
+//         return (int) Math.sqrt(number) == Math.sqrt(number)? divCnt + 1 : divCnt;
+//     }
+// }
