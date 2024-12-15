5fa916b0f0d6e1d6c09034e9d0964d6e7d881b20
Meow-Snow <MeowSnow7771@gmail.com>
2024-12-16 02:49:43 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.46 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -10,3 +10,14 @@ class Solution {
         return String.join("", strList);
     }
 }
+
+
+
+// LinkedHashSet을 이용하면 간단하게 해결할 수 있다
+// import java.util.*;
+
+// class Solution {
+//     public String solution(String my_string) {
+//         return String.join("", new LinkedHashSet<String>(Arrays.asList(my_string.split(""))));
+//     }
+// }
