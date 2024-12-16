b5826b020c471ac7d405bafecc14b86d3718aec2
Meow-Snow <MeowSnow7771@gmail.com>
2024-12-17 00:58:27 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.16 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.java
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.java
@@ -8,3 +8,16 @@ class Solution {
         return answer;
     }
 }
+
+
+
+// class Solution {
+//     public int solution(String myString, String pat) {
+//         int answer = 0;
+//         for (int i = 0; i < myString.length() - pat.length() + 1; i++) {
+//             if (myString.substring(i).startsWith(pat)) answer++;
+//         }
+        
+//         return answer;
+//     }
+// }
