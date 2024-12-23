4fbcc3128f6829962426bf24c8f4c6fa68e0155e
Meow-Snow <MeowSnow7771@gmail.com>
2024-12-23 10:03:49 +0900

    [level 0] Title: 팩토리얼, Time: 0.02 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -1,11 +1,26 @@
 class Solution {
     public int solution(int n) {
-        int product = 1;
-        for (int i = 1; i <= 11; i++) {
-            product *= i;
-            if (product > n) return i - 1;
+        int answer = 1;
+        int factorial = 1;
+        while (factorial <= n) {
+            answer++;
+            factorial *= answer;
         }
         
-        return -1;
+        return answer - 1;
     }
 }
+
+
+
+// class Solution {
+//     public int solution(int n) {
+//         int product = 1;
+//         for (int i = 1; i <= 11; i++) {
+//             product *= i;
+//             if (product > n) return i - 1;
+//         }
+        
+//         return -1;
+//     }
+// }
