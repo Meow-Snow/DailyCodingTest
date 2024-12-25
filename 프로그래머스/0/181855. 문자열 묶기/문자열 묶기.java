645452d3292adbbe4540b3861813040f407305e1
Meow-Snow <MeowSnow7771@gmail.com>
2024-12-25 11:02:17 +0900

    [level 0] Title: 문자열 묶기, Time: 29.39 ms, Memory: 132 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -1,15 +1,35 @@
+import java.util.*;
+
 class Solution {
     public int solution(String[] strArr) {
-        int[] strLenArr = new int[100000];
+        Map<Integer, Integer> strLenMap = new HashMap<>();
         for (String str : strArr) {
-            strLenArr[str.length()]++;
+            strLenMap.put(str.length(), strLenMap.getOrDefault(str.length(), 0) + 1);
         }
         
         int answer = 0;
-        for (int strLen : strLenArr) {
-            answer = Math.max(answer, strLen);
+        for (Integer key : strLenMap.keySet()) {
+            answer = Math.max(answer, strLenMap.get(key));
         }
         
         return answer;
     }
 }
+
+
+
+// class Solution {
+//     public int solution(String[] strArr) {
+//         int[] strLenArr = new int[100000];
+//         for (String str : strArr) {
+//             strLenArr[str.length()]++;
+//         }
+        
+//         int answer = 0;
+//         for (int strLen : strLenArr) {
+//             answer = Math.max(answer, strLen);
+//         }
+        
+//         return answer;
+//     }
+// }
