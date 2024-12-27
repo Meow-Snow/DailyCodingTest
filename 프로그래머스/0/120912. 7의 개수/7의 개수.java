87a4d32aae9d637054753c43f58714b4936a2af0
Meow-Snow <MeowSnow7771@gmail.com>
2024-12-28 01:32:33 +0900

    [level 0] Title: 7의 개수, Time: 0.05 ms, Memory: 81.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        for (int num : array) {
+           for (char c : String.valueOf(num).toCharArray()) {
+               if (c == '7') answer++;
+           }
+        }
+        
+        return answer;
+    }
+}
