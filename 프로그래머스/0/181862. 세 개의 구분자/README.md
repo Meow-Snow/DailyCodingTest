2b211d1a58285a05063b29cdabdb232869439462
Meow-Snow <MeowSnow7771@gmail.com>
2024-12-29 17:07:07 +0900

    [level 0] Title: 세 개의 구분자, Time: 80.43 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/README.md
+++ 프로그래머스/0/181862. 세 개의 구분자/README.md
@@ -1,10 +1,10 @@
 # [level 0] 세 개의 구분자 - 181862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181862#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181862) 
 
 ### 성능 요약
 
-메모리: 112 MB, 시간: 48.10 ms
+메모리: 102 MB, 시간: 80.43 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 26일 17:17:29
+2024년 12월 29일 17:07:02
 
 ### 문제 설명
 
--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -1,9 +1,19 @@
-import java.util.Arrays;
-
 class Solution {
     public String[] solution(String myStr) {
-        String[] strArr = myStr.split("[abc]+");
+        String[] strArr = myStr.replaceAll("[abc]+", " ").trim().split(" ");
         
-        return (strArr.length == 0)? new String[]{"EMPTY"} : (strArr[0].isEmpty())? Arrays.copyOfRange(strArr, 1, strArr.length) : strArr;
+        return (strArr[0].isEmpty())? new String[]{"EMPTY"} : strArr;
     }
 }
+
+
+
+// import java.util.Arrays;
+
+// class Solution {
+//     public String[] solution(String myStr) {
+//         String[] strArr = myStr.split("[abc]+");
+        
+//         return (strArr.length == 0)? new String[]{"EMPTY"} : (strArr[0].isEmpty())? Arrays.copyOfRange(strArr, 1, strArr.length) : strArr;
+//     }
+// }
