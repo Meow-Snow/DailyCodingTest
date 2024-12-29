0be3aa7cf5605a846794f9d8fdb4f3968c190fdb
Meow-Snow <MeowSnow7771@gmail.com>
2024-12-30 01:47:32 +0900

    [level 0] Title: 리스트 자르기, Time: 0.04 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -1,19 +1,37 @@
 class Solution {
-    private static int startIdx, endIdx, interval = 1;
     public int[] solution(int n, int[] slicer, int[] num_list) {
-        int[] startIdxArr = {0, slicer[0], slicer[0], slicer[0]};
-        int[] endIdxArr = {slicer[1], num_list.length - 1, slicer[1], slicer[1]};
-        int[] intervalArr = {1, 1, 1, slicer[2]};
+        int idx1 = (n == 1)? 0 : slicer[0];
+        int idx2 = (n == 2)? num_list.length - 1 : slicer[1];
+        int step = (n == 4)? slicer[2] : 1;
         
-        int startIdx = startIdxArr[n - 1];
-        int endIdx = endIdxArr[n - 1];
-        int interval = intervalArr[n - 1];
-                
-        int[] answer = new int[(endIdx - startIdx) / interval + 1];
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = num_list[startIdx + interval * i];
+        int[] answer = new int[(idx2 - idx1) / step + 1];
+        int idx = 0;
+        for (int i = idx1; i <= idx2; i += step) {
+            answer[idx++] = num_list[i];
         }
         
         return answer;
     }
 }
+
+
+
+// class Solution {
+//     private static int startIdx, endIdx, interval = 1;
+//     public int[] solution(int n, int[] slicer, int[] num_list) {
+//         int[] startIdxArr = {0, slicer[0], slicer[0], slicer[0]};
+//         int[] endIdxArr = {slicer[1], num_list.length - 1, slicer[1], slicer[1]};
+//         int[] intervalArr = {1, 1, 1, slicer[2]};
+        
+//         int startIdx = startIdxArr[n - 1];
+//         int endIdx = endIdxArr[n - 1];
+//         int interval = intervalArr[n - 1];
+                
+//         int[] answer = new int[(endIdx - startIdx) / interval + 1];
+//         for (int i = 0; i < answer.length; i++) {
+//             answer[i] = num_list[startIdx + interval * i];
+//         }
+        
+//         return answer;
+//     }
+// }
