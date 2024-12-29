401166a947cb129ff58d54c7d6c97dd0640e115d
Meow-Snow <MeowSnow7771@gmail.com>
2024-12-30 02:00:29 +0900

    [level 0] Title: qr code, Time: 0.11 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.java
+++ 프로그래머스/0/181903. qr code/qr code.java
@@ -1,10 +1,24 @@
+// 조금 더 숙고했다면 더 간단한 코드로 해결할 수 있었다
 class Solution {
     public String solution(int q, int r, String code) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < code.length(); i++) {
-            if (i % q == r) sb.append(code.charAt(i));
+        for (int i = r; i < code.length(); i += q) {
+            sb.append(code.charAt(i));
         }
         
         return sb.toString();
     }
 }
+
+
+
+// class Solution {
+//     public String solution(int q, int r, String code) {
+//         StringBuilder sb = new StringBuilder();
+//         for (int i = 0; i < code.length(); i++) {
+//             if (i % q == r) sb.append(code.charAt(i));
+//         }
+        
+//         return sb.toString();
+//     }
+// }
