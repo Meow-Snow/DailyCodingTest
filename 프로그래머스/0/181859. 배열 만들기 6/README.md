f4abe37f7919a1cea446839926d1b08917534799
Meow-Snow <MeowSnow7771@gmail.com>
2025-01-05 02:47:01 +0900

    [level 0] Title: 배열 만들기 6, Time: 44.32 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/0/181859. 배열 만들기 6/README.md
+++ 프로그래머스/0/181859. 배열 만들기 6/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열 만들기 6 - 181859 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181859) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181859#) 
 
 ### 성능 요약
 
-메모리: 125 MB, 시간: 46.31 ms
+메모리: 81.3 MB, 시간: 44.32 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 05일 02:25:43
+2025년 01월 05일 02:46:58
 
 ### 문제 설명
 
--- 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -1,13 +1,29 @@
-import java.util.*;
+import java.util.Stack;
 
 class Solution {
-    public Integer[] solution(int[] arr) {
+    public int[] solution(int[] arr) {
         Stack<Integer> stk = new Stack<>();
         for (int num : arr) {
-            if (stk.isEmpty() || stk.peek() != num) stk.add(num);
+            if (stk.isEmpty() || stk.peek() != num) stk.push(num);
             else stk.pop();
         }
         
-        return stk.isEmpty()? new Integer[]{-1} : stk.toArray(Integer[]::new);
+        return stk.isEmpty()? new int[]{-1} : stk.stream().mapToInt(i -> i).toArray();
     }
 }
+
+
+
+// import java.util.*;
+
+// class Solution {
+//     public Integer[] solution(int[] arr) {
+//         Stack<Integer> stk = new Stack<>();
+//         for (int num : arr) {
+//             if (stk.isEmpty() || stk.peek() != num) stk.push(num);
+//             else stk.pop();
+//         }
+
+//         return stk.isEmpty()? new Integer[]{-1} : stk.toArray(Integer[]::new);
+//     }
+// }
