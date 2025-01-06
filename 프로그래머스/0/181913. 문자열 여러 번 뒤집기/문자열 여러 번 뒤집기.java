8bb3554c004bb7c287498cd97d35506a2022895e
Meow-Snow <MeowSnow7771@gmail.com>
2025-01-07 01:33:18 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 2.62 ms, Memory: 85.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string, int[][] queries) {
+        StringBuilder sb = new StringBuilder(my_string);
+        for (int[] query : queries) {       
+            sb.replace(query[0], query[1] + 1, new StringBuilder(sb.substring(query[0], query[1] + 1)).reverse().toString());
+        }
+        
+        return sb.toString();
+    }
+}
