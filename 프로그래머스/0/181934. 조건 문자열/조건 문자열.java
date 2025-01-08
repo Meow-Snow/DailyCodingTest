ee035298bf6950e87bd30fcd0e996de26dbd23c5
Meow-Snow <MeowSnow7771@gmail.com>
2025-01-09 07:30:03 +0900

    [level 0] Title: 조건 문자열, Time: 0.02 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
@@ -1,8 +1,16 @@
 class Solution {
     public int solution(String ineq, String eq, int n, int m) {
-        if (ineq.equals(">") && n > m) return 1;
-        else if (ineq.equals("<") && n < m) return 1;
-            
-        return (eq.equals("=") && n == m)?  1 : 0;
+        return ((ineq.equals(">")? n > m : n < m) || (eq.equals("=")? n == m : false))? 1 : 0;
     }
 }
+
+
+
+// class Solution {
+//     public int solution(String ineq, String eq, int n, int m) {
+//         if (ineq.equals(">") && n > m) return 1;
+//         else if (ineq.equals("<") && n < m) return 1;
+            
+//         return (eq.equals("=") && n == m)?  1 : 0;        
+//     }
+// }
