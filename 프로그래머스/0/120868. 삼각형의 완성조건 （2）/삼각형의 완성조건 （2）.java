908a676b02f1e9982c630b55290a0f9703d83fc7
Meow-Snow <MeowSnow7771@gmail.com>
2025-01-09 14:43:18 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.07 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
@@ -1,5 +1,13 @@
 class Solution {
     public int solution(int[] sides) {
-        return sides[0] + sides[1] - Math.abs(sides[0] - sides[1]) - 1;
+        return Math.min(sides[0], sides[1]) * 2 - 1;
     }
 }
+
+
+
+// class Solution {
+//     public int solution(int[] sides) {
+//         return sides[0] + sides[1] - Math.abs(sides[0] - sides[1]) - 1;
+//     }
+// }
