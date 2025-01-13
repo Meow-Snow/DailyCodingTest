1b859749aa0c341856b1dbc933d3bfc1ed3999c6
Meow-Snow <MeowSnow7771@gmail.com>
2025-01-14 02:29:40 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.04 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.java
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.java
@@ -1,15 +1,16 @@
 class Solution {
     public int solution(int[][] dots) {
-        int[] dxMax = {Integer.MAX_VALUE, Integer.MIN_VALUE};
-        int[] dyMax = {Integer.MAX_VALUE, Integer.MIN_VALUE};
-        for (int[] dot : dots) {
-            dxMax[0] = Math.min(dxMax[0], dot[0]);
-            dxMax[1] = Math.max(dxMax[1], dot[0]);
-            dyMax[0] = Math.min(dyMax[0], dot[1]);
-            dyMax[1] = Math.max(dyMax[1], dot[1]);
+        int[] delta = new int[2];
+        int[] maxDelta = new int[2];
+        
+        for (int i = 1; i < dots.length; i++) {
+            delta = new int[]{Math.abs(dots[0][0] - dots[i][0]), Math.abs(dots[0][1] - dots[i][1])};
+            if (maxDelta[0] + maxDelta[1] < delta[0] + delta[1]) {
+                maxDelta = delta;
+            }
         }
         
-        return Math.abs(dxMax[1] - dxMax[0]) * Math.abs(dyMax[1] - dyMax[0]);
+        return maxDelta[0] * maxDelta[1];
     }
 }
 
@@ -17,12 +18,15 @@ class Solution {
 
 // class Solution {
 //     public int solution(int[][] dots) {
-//         int mnhtDist = 0;
-//         int[] pos = dots[0];
+//         int[] dxMax = {Integer.MAX_VALUE, Integer.MIN_VALUE};
+//         int[] dyMax = {Integer.MAX_VALUE, Integer.MIN_VALUE};
 //         for (int[] dot : dots) {
-//             mnhtDist = Math.max(Math.abs(dot[0] - dots[0][0]) + Math.abs(dot[1] - dots[0][1]), mnhtDist);
+//             dxMax[0] = Math.min(dxMax[0], dot[0]);
+//             dxMax[1] = Math.max(dxMax[1], dot[0]);
+//             dyMax[0] = Math.min(dyMax[0], dot[1]);
+//             dyMax[1] = Math.max(dyMax[1], dot[1]);
 //         }
         
-//         return (mnhtDist / 2) * (mnhtDist / 2);
+//         return Math.abs(dxMax[1] - dxMax[0]) * Math.abs(dyMax[1] - dyMax[0]);
 //     }
 // }
