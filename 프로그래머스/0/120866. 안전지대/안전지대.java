770a5487dd7dbcab0c8e1d8c9cc72e1c18d9012b
Meow-Snow <MeowSnow7771@gmail.com>
2025-01-25 00:33:30 +0900

    [level 0] Title: 안전지대, Time: 0.07 ms, Memory: 82.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120866. 안전지대/안전지대.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int solution(int[][] board) {
+        boolean[][] flags = new boolean[board.length][board[0].length];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                if (board[i][j] == 1) {
+                    for (int k = i - 1; k <= i + 1; k++) {
+                        for (int l = j - 1; l <= j + 1; l++) {
+                            if (isOnTheBoard(board, k, l)) flags[k][l] = true;
+                        }
+                    }
+                }
+            }
+        }
+                
+        return cntDangerZones(flags);
+    }
+    
+    private boolean isOnTheBoard(int[][] board, int l, int w) {
+        if (l < 0 || l >= board.length || w < 0 || w >= board[0].length) return false;
+        
+        return true;
+    }
+    
+    private int cntDangerZones(boolean[][] flags) {
+        int cnt = 0;
+        for (int i = 0; i < flags.length; i++) {
+            for (int j = 0; j < flags[0].length; j++) {
+                if (!flags[i][j]) cnt++;
+            }
+        }
+        
+        return cnt;
+    }
+}
