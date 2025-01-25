6d1d95743eb456e21882ba33324e8e7da8b76075
Meow-Snow <MeowSnow7771@gmail.com>
2025-01-26 04:23:48 +0900

    [level 0] Title: 주사위 게임 3, Time: 1.30 ms, Memory: 84.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int a, int b, int c, int d) {        
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(a, map.getOrDefault(a, 0) + 1);
+        map.put(b, map.getOrDefault(b, 0) + 1);
+        map.put(c, map.getOrDefault(c, 0) + 1);
+        map.put(d, map.getOrDefault(d, 0) + 1);
+        
+        List<int[]> list = new ArrayList<>();
+        for (int key : map.keySet()) {
+            list.add(new int[]{map.get(key), key});
+        }
+        
+        Collections.sort(list, (x, y) -> Integer.compare(x[0], y[0]));
+        int num1 = list.get(0)[1];
+        int num2 = (list.size() > 1)? list.get(1)[1] : 0;
+        
+        switch (map.keySet().size()) {
+                case 1 -> {return 1111 * a;}
+                case 2 -> {if (list.get(0)[0] != list.get(1)[0])
+                    return (int) Math.pow(10 * num2 + num1, 2);}
+                case 3 -> {return num1 * num2;}
+                case 4 -> {return Math.min(Math.min(a, b), Math.min(c, d));}
+        }
+        
+        return (num1 + num2) * Math.abs(num1 - num2);
+    }
+}
