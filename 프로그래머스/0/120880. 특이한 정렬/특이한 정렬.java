2fa0cc7f23aaafeedbc3b5b8df543a52c06d5537
Meow-Snow <MeowSnow7771@gmail.com>
2025-01-29 23:14:00 +0900

    [level 0] Title: 특이한 정렬, Time: 5.81 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
@@ -1,14 +1,28 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] numlist, int n) {
-        Integer[] nums = Arrays.stream(numlist).boxed().toArray(Integer[]::new);
-        Arrays.sort(nums, (a, b) -> {
-            int intComp = Integer.compare(Math.abs(a - n), Math.abs(b - n));
-            if (intComp == 0) return Integer.compare(b, a);
-            else return intComp;
-        });
-        
-        return Arrays.stream(nums).mapToInt(i -> i).toArray();
+        return Arrays.stream(numlist)
+            .boxed()
+            .sorted((a, b) -> Math.abs(a - n) == Math.abs(b - n)? Integer.compare(b, a) : Integer.compare(Math.abs(a - n), Math.abs(b - n)))
+            .mapToInt(i -> i)
+            .toArray();
     }
 }
+
+
+
+// import java.util.*;
+
+// class Solution {
+//     public int[] solution(int[] numlist, int n) {
+//         Integer[] nums = Arrays.stream(numlist).boxed().toArray(Integer[]::new);
+//         Arrays.sort(nums, (a, b) -> {
+//             int intComp = Integer.compare(Math.abs(a - n), Math.abs(b - n));
+//             if (intComp == 0) return Integer.compare(b, a);
+//             else return intComp;
+//         });
+        
+//         return Arrays.stream(nums).mapToInt(i -> i).toArray();
+//     }
+// }
