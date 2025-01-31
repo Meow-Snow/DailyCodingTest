0886d3729cd707c6f64c43bcbfe9f54c822df04a
Meow-Snow <MeowSnow7771@gmail.com>
2025-02-01 02:10:36 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.03 ms, Memory: 71.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -0,0 +1,30 @@
+class Solution {
+    private static final int[] primeFactors = {2, 5};
+    
+    public int solution(int a, int b) {
+        b /= getGCD(a, b);        
+        for (int primeFactor : primeFactors) {
+            b = getSimplified(b, primeFactor);
+        }
+        
+        return (b == 1)? 1 : 2;
+    }
+    
+    private int getGCD(int num1, int num2) {
+        while (num2 > 0) {
+            int temp = num2;
+            num2 = num1 % num2; 
+            num1 = temp;
+        }
+        
+        return num1;
+    }
+    
+    private int getSimplified(int num1, int num2) {
+        while (num1 % num2 == 0) {
+            num1 /= num2;
+        }
+        
+        return num1;
+    }
+}
