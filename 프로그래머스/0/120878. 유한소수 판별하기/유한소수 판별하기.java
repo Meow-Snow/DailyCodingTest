04d38844c1ae26aa3b089745378c3cf3ebe33194
Meow-Snow <MeowSnow7771@gmail.com>
2025-02-01 05:52:27 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.04 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -10,14 +10,18 @@ class Solution {
         return (b == 1)? 1 : 2;
     }
     
-    private int getGCD(int num1, int num2) {
-        while (num2 > 0) {
-            int temp = num2;
-            num2 = num1 % num2; 
-            num1 = temp;
-        }
+//     private int getGCD(int num1, int num2) {
+//         while (num2 > 0) {
+//             int temp = num2;
+//             num2 = num1 % num2; 
+//             num1 = temp;
+//         }
         
-        return num1;
+//         return num1;
+//     }
+    
+    private int getGCD(int num1, int num2) {
+        return (num2 == 0)? num1 : getGCD(num2, num1 % num2);
     }
     
     private int getSimplified(int num1, int num2) {
